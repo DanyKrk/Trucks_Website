1f97e4e2928522505f1866eb515191854db4d770
DanyKrk <daniel.k.8576@gmail.com>
2023-02-08 19:51:22 +0000

    Add services

---
+++ src/app/services/truck.service.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { TRUCKS_API_URL } from '../../interfaces/constants'
+import { TruckService } from './truck.service';
+
+describe('TruckService', () => {
+  let service: TruckService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [ HttpClientTestingModule ]
+    });
+    service = TestBed.inject(TruckService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return a truck result', () => {
+    service.getTrucks().subscribe(result => {
+      expect(result).toBeTruthy();
+      expect(result.trucks).toBeTruthy();
+    });
+    const req = httpMock.expectOne(TRUCKS_API_URL);
+    expect(req.request.method).toBe('GET')
+  })
+});
